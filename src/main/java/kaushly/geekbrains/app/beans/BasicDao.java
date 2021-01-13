4ffbedb6b2b99b8c6cab5e664c2c1f04f98e1418
Каушлы Сергей <Kaushly@yandex.ru>
2021-01-13 23:50:44 +0300

    Lesson 6 В базе данных необходимо реализовать возможность хранить информацию о покупателях (id, имя) и товарах (id, название, стоимость). У каждого покупателя свой набор купленных товаров; Для обеих сущностей создаете Dao классы. Работу с SessionFactory выносите во вспомогательный класс; * Создаете сервис, позволяющий по id покупателя узнать список купленных им товаров, и по id товара узнавать список покупателей этого товара;

--- src/main/java/kaushly/geekbrains/app/beans/BasicDao.java
+++ src/main/java/kaushly/geekbrains/app/beans/BasicDao.java
@@ -21,7 +21,7 @@ public abstract class BasicDao<T> {
     public T findById(long id, Session session) {
         T item = (T) session.get(cls, id);
         if (item == null) {
-            throw new RuntimeException("Product not found");
+            throw new RuntimeException(cls.getSimpleName() + " not found");
         } else {
             return item;
         }
@@ -30,28 +30,11 @@ public abstract class BasicDao<T> {
     public List<T> findAll(Session session) {
         List<T> items = session.createQuery(String.format("FROM %s",cls.getSimpleName()), cls).getResultList();
         if (items == null) {
-            throw new RuntimeException("Products not found");
+            throw new RuntimeException(cls.getSimpleName() + " not found");
         } else {
             return items;
         }
     }
 
-//    public void deleteById(long id) {
-//        try (Session session = factory.getCurrentSession()) {
-//            session.beginTransaction();
-//            T item = (T) session.get(cls, id);
-//            session.delete(item);
-//            session.getTransaction().commit();
-//        } catch (NoResultException e) {
-//            throw new RuntimeException("Client not found");
-//        }
-//    }
 
-//    public void saveOrUpdate(T item) {
-//        try (Session session = factory.getCurrentSession()) {
-//            session.beginTransaction();
-//            session.saveOrUpdate(item);
-//            session.getTransaction().commit();
-//        }
-//    }
 }
